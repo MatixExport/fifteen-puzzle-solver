f98b26c35fe3c68cb7a58883138774cbc9a2861d
Mateusz Giełczyński <mateusz.gielczynski@outlook.com>
2024-04-27 15:28:49 +0200

    add: bfs search working

--- src/main.py
+++ src/main.py
@@ -4,14 +4,14 @@ from Board import Board
 from src.Index import Index
 
 if __name__ == "__main__":
-    board = Board(4, 4)
+    board = Board(3, 3)
 
     board.generate_random()
 
     board.set_table(np.array([[1, 6, 2, 4], [5, 13, 3, 7], [10, 0, 11, 8], [9, 14, 15, 12]]))
     print(board.is_solved())
 
-    board.depth_recursion(0, Index(0, 0))
+    board.bfs()
 
     print(board.is_solved())
     print(board)
