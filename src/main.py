a0fd0ad58276aad03c35da16c572bc17672f8c7a
Mateusz Giełczyński <mateusz.gielczynski@outlook.com>
2024-04-27 15:48:46 +0200

    add: main

--- src/main.py
+++ src/main.py
@@ -4,14 +4,16 @@ from Board import Board
 from src.Index import Index
 
 if __name__ == "__main__":
-    board = Board(3, 3)
+    board = Board(4, 4)
+    board2 = Board(3, 3)
+    board3 = Board(3, 3)
 
-    board.generate_random()
+
 
     board.set_table(np.array([[1, 6, 2, 4], [5, 13, 3, 7], [10, 0, 11, 8], [9, 14, 15, 12]]))
-    print(board.is_solved())
+    board2.set_table(np.array([[1,2,3], [4,5,6], [7,0,8]]))
+    board3.set_table(np.array([[2,5,3], [1,0,6], [4,7,8]]))
 
-    board.bfs()
 
-    print(board.is_solved())
-    print(board)
+    board3.bfs()
+    print(board3)
