f48943f7ef97d44e805eba947298f4cc648c9994
Jakub Kubiś <jakubkubis45@gmail.com>
2024-04-28 18:47:19 +0200

    UPDATE tests

--- src/main.py
+++ src/main.py
@@ -6,26 +6,7 @@ from Board import Board
 from src.Index import Index
 
 if __name__ == "__main__":
-    board = Board()
-    board2 = Board()
-    board3 = Board()
-    board4 = Board()
-
-    board.set_table(np.array([[1, 6, 2, 4], [5, 13, 3, 7], [10, 0, 11, 8], [9, 14, 15, 12]]))
-    board2.set_table(np.array([[1, 2, 3], [4, 5, 6], [7, 0, 8]]))
-    board3.set_table(np.array([[2, 5, 3], [1, 0, 6], [4, 7, 8]]))
-    board3.set_table(np.array([[5, 2, 7], [8, 4, 0], [1, 3, 6]]))
-    board4.set_table(np.array([[1, 5, 0], [4, 3, 2], [7, 8, 6]]))
-    board4.set_table(np.array([[1, 0, 5], [4, 3, 2], [7, 8, 6]]))
-    board4.set_table(np.array([[1, 2, 7,3], [5, 10, 6,4], [9,0,11, 8],[13,14,15,12]]))
-
-    # print(board.a_star(board.manhattan_dist))
-    # print(board.dfs(0, None))
-    # print(board.bfs())
-
-    # print(board3.manhattan_dist())
-    # print(board3.hamming_dist())
-    print(board)
+    pass
 
 
 
--- src/test/test_Board.py
+++ src/test/test_Board.py
@@ -2,12 +2,10 @@ from unittest import TestCase
 
 import numpy as np
 
-from src import bfs_solver, dfs_solver, a_star_solver
 from src.AstarSolver import AstarSolver
 from src.BfsSolver import BfsSolver
 from src.Board import Board
 from src.DfsSolver import DfsSolver
-from src.a_star_solver import hamming_dist, manhattan_dist
 
 
 class TestBoard(TestCase):
@@ -16,10 +14,10 @@ class TestBoard(TestCase):
         board2 = Board()
         board1.set_table(np.array([[1, 2, 3, 4], [5, 6, 11, 7], [9, 10, 0, 8], [13, 14, 15, 12]]))
         board2.set_table(np.array([[1, 0, 5], [4, 3, 2], [7, 8, 6]]))
-        print(dfs_solver.solve(board2))
+        DfsSolver(board2).solve()
         assert np.array_equal(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]]), board2.table)
 
-        dfs_solver.solve(board1)
+        DfsSolver(board1).solve()
         assert np.array_equal(np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]), board1.table)
 
     def test_bfs(self):
@@ -27,10 +25,10 @@ class TestBoard(TestCase):
         board2 = Board()
         board1.set_table(np.array([[1, 2, 3, 4], [5, 6, 11, 7], [9, 10, 0, 8], [13, 14, 15, 12]]))
         board2.set_table(np.array([[1, 0, 5], [4, 3, 2], [7, 8, 6]]))
-        bfs_solver.solve(board2)
+        BfsSolver(board2).solve()
         assert np.array_equal(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]]), board2.table)
 
-        bfs_solver.solve(board1)
+        BfsSolver(board1).solve()
         assert np.array_equal(np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]),
                               board1.table)
 
@@ -40,10 +38,10 @@ class TestBoard(TestCase):
         board1.set_table(np.array([[1, 2, 3, 4], [5, 6, 11, 7], [9, 10, 0, 8], [13, 14, 15, 12]]))
         board2.set_table(np.array([[1, 0, 5], [4, 3, 2], [7, 8, 6]]))
 
-        a_star_solver.a_star(board2, hamming_dist)
+        AstarSolver(board2, AstarSolver.hamming_dist).solve()
         assert np.array_equal(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]]), board2.table)
 
-        a_star_solver.a_star(board1, hamming_dist)
+        AstarSolver(board1, AstarSolver.hamming_dist).solve()
         assert np.array_equal(np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]),
                               board1.table)
 
@@ -53,10 +51,10 @@ class TestBoard(TestCase):
         board1.set_table(np.array([[1, 2, 3, 4], [5, 6, 11, 7], [9, 10, 0, 8], [13, 14, 15, 12]]))
         board2.set_table(np.array([[1, 0, 5], [4, 3, 2], [7, 8, 6]]))
 
-        a_star_solver.a_star(board2, manhattan_dist)
+        AstarSolver(board2, AstarSolver.manhattan_dist).solve()
         assert np.array_equal(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]]), board2.table)
 
-        a_star_solver.a_star(board1, manhattan_dist)
+        AstarSolver(board1, AstarSolver.manhattan_dist).solve()
         assert np.array_equal(np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]),
                               board1.table)
 
@@ -79,11 +77,6 @@ class TestBoard(TestCase):
         solver.solve()
         assert np.array_equal(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]]), board.table)
         board.set_table(np.array([[1, 0, 5], [4, 3, 2], [7, 8, 6]]))
-        solver = AstarSolver(board,AstarSolver.manhattan_dist)
+        solver = AstarSolver(board, AstarSolver.manhattan_dist)
         solver.solve()
         assert np.array_equal(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]]), board.table)
-
-
-
-
-
