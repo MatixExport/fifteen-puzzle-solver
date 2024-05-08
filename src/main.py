fb3789ff7eef9113510ad5a9ef7f391a91b54622
Jakub Kubiś <jakubkubis45@gmail.com>
2024-05-08 21:15:43 +0200

    ADD additional information output, logger info parsing

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from BfsSolver import BfsSolver
 from Board import Board
 from DfsSolver import DfsSolver
 from AstarSolver import AstarSolver
+from Logger import Logger
 
 msg = "Program for finding integral of given functions."
 
@@ -16,37 +17,30 @@ if __name__ == "__main__":
     parser.add_argument('Source', help='Name of file containing starting puzzle')
     parser.add_argument('Output', help='Name of file that the result will be saved to')
     parser.add_argument('Output_info', help='Name of file that will contain additional information')
-    parser.add_argument("-d","--depth",type=int, help="max depth, used in dfs and bfs")
+    parser.add_argument("-d", "--depth", type=int, help="max depth, used in dfs and bfs")
 
     args = parser.parse_args()
     max_depth = 15
 
+    board = Board()
+    logger = Logger()
+    board.read_from_file(args.Source)
+
+    output = open(args.Output, "w+")
+    solver = DfsSolver(board)
+
     if args.depth:
         max_depth = args.depth
 
-    board = Board()
-    board.read_from_file(args.Source)
-    output = open(args.Output, "w+")
-
     if args.Strategy == "dfs":
-        board.set_order(args.Parameter)
         solver = DfsSolver(board)
         solver.set_max_depth(max_depth)
-        if solution := solver.solve():
-            output.write(str(len(solution)) + "\n")
-            output.write(solution)
-        else:
-            output.write("-1")
+        board.set_order(args.Parameter)
 
     if args.Strategy == "bfs":
-        board.set_order(args.Parameter)
         solver = BfsSolver(board)
         solver.set_max_depth(max_depth)
-        if solution := solver.solve():
-            output.write(str(len(solution)) + "\n")
-            output.write(solution)
-        else:
-            output.write("-1")
+        board.set_order(args.Parameter)
 
     if args.Strategy == "astr":
         if args.Parameter == "hamm":
@@ -54,9 +48,13 @@ if __name__ == "__main__":
         else:
             solver = AstarSolver(board, AstarSolver.manhattan_dist)
 
-        if solution := solver.solve():
-            output.write(str(len(solution)) + "\n")
-            output.write(solution)
-            print(board)
-        else:
-            output.write("-1")
+    logger.set_solver(solver)
+    if solution := logger.recorded_solve():
+        output.write(str(len(solution)) + "\n")
+        output.write(solution)
+    else:
+        output.write("-1")
+
+    output = open(args.Output_info, "w+")
+    output.write(logger.parse_recorded_information())
+
